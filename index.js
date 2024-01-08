58ced642b82671853de468e35f6c25ecdba68790
hoodaatwa <31570775+hoodaatwa@users.noreply.github.com>
2024-01-08 20:41:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
   // Show or hide the button based on scroll position
 
   document.getElementById("container").onscroll = function() {
@@ -85,6 +86,8 @@
     codeTag.innerHTML = '&lt;style id="scrollToTopBtnStyle"&gt;\n #scrollToTopBtn {\n display: none;\n position: absolute; /* Set to absolute position */\n border: none;\n cursor: pointer;\n text-align: center;\n right: ' + positionRightInput + ';\n left: ' + positionLeftInput + ';\n bottom: ' + positionBottomInput + ';\n padding: ' + paddingInput + ';\n color: ' + colorInput + ';\n background-color: ' + bgColorInput + ';\n width: ' + widthInput + ';\n height: ' + heightInput + ';\n border-radius: ' + borderRadiusInput + ';\n }\n&lt;/style&gt;\n&lt;button id="scrollToTopBtn" onclick="scrollToTop()"&gt;&lt;i class="fas fa-arrow-up"&gt;&lt;/i&gt;&lt;/button&gt;\n&lt;script&gt;\n // Show or hide the button based on scroll position\n window.onscroll = function() {\n var scrollToTopBtn = document.getElementById("scrollToTopBtn");\n if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {\n scrollToTopBtn.style.display = "block";\n } else {\n scrollToTopBtn.style.display = "none";\n }\n };\n\n // Scroll to the top of the page\n function scrollToTop() {\n document.body.scrollTop = 0; // For Safari\n document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera\n }\n&lt;/script&gt;';
 
   }
+
+
 
   const currentPageLocation = window.location.href;
 
@@ -223,3 +226,4 @@
     }
 
   }
+
